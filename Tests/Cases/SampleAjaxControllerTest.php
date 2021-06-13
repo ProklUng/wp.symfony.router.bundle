9b3d03b2fd76025c63f1be843563881afe834c64
ProklUng <funtik21975@gmail.com>
2021-06-13 14:54:39 +0300

    Тесты.

--- Tests/Cases/SampleAjaxControllerTest.php
+++ Tests/Cases/SampleAjaxControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Prokl\WpSymfonyRouterBundle\Tests\Cases;
 
+use Prokl\TestingTools\Tools\Container\BuildContainer;
 use Prokl\WordpressCi\Base\WordpressableAjaxTestCase;
 
 /**
@@ -10,16 +11,41 @@ use Prokl\WordpressCi\Base\WordpressableAjaxTestCase;
  */
 class SampleAjaxControllerTest extends WordpressableAjaxTestCase
 {
+    /**
+     * @inheritDoc
+     */
     protected function setUp() : void
     {
         parent::setup();
         wp_set_current_user( 1 );
+
+        $this->container = static::$testContainer = BuildContainer::getTestContainer(
+            [
+                'test_container.yaml'
+            ],
+            '/../../../../Tests/Fixture'
+        );
+
+        $this->container->get('wp_ajax.initializer');
     }
 
+    /**
+     * @return void
+     */
     public function testAction() : void
     {
         $this->_handleAjax('examples_wp');
         $result = $this->_last_response;
         $this->assertSame('OK', $result);
+    }
+
+    /**
+     * @return void
+     */
+    public function testActionInvalid() : void
+    {
+        $this->_handleAjax('examples_invalid');
+        $result = $this->_last_response;
+        $this->assertSame('', $result);
     }
 }
