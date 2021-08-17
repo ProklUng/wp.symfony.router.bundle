bc065ba39db606083ba77a0f57adc06a9c57cd87
ProklUng <funtik21975@gmail.com>
2021-08-17 20:13:22 +0300

    LoaderBundleRoutesTrait

---
+++ Services/Utils/LoaderBundleRoutesTrait.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Prokl\WpSymfonyRouterBundle\Services\Utils;
+
+use InvalidArgumentException;
+use Prokl\WpSymfonyRouterBundle\Services\Router\InitRouter;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Routing\Loader\YamlFileLoader;
+
+/**
+ * Trait LoaderBundleRoutesTrait
+ * @package Prokl\WpSymfonyRouterBundle\Services\Utils
+ * @since 17.08.2021
+ */
+trait LoaderBundleRoutesTrait
+{
+    /**
+     * Загрузить роуты в бандле.
+     *
+     * @param string $path   Путь к конфигу.
+     * @param string $config Конфигурационный файл.
+     *
+     * @return void
+     *
+     * @throws InvalidArgumentException Нет класса-конфигуратора роутов.
+     */
+    private function loadRoutes(string $path, string $config = 'routes.yaml') : void
+    {
+        $routeLoader = new YamlFileLoader(
+            new FileLocator($path)
+        );
+
+        $routes = $routeLoader->load($config);
+
+        if (class_exists(InitRouter::class)) {
+            InitRouter::addRoutesBundle($routes);
+            return;
+        }
+
+        throw new InvalidArgumentException('Class InitRouter not exist.');
+    }
+}
